3522775abc1660da6eb403a50154ad1b98bbaf58
aizazalisyed <aizazalisyed@outlook.com>
2024-07-23 01:13:15 +0500

    added module refrences for each test case in file_system.c

--- Filesystem/file_system.c
+++ Filesystem/file_system.c
@@ -1,10 +1,17 @@
 #include "Modules/freevxfs_check.h"
 #include "Modules/cramfs_check.h"
 
+// Color definition for yellow
+#define YELLOW "\033[0;33m"
+
 int main()
 {
-
+    // Heading for cramfs check
+    printf(YELLOW "\n1) Ensure cramfs kernel module is not available:\n" RESET);
     run_cramfs_checks();
+
+    // Heading for freevxfs check
+    printf(YELLOW "\n2) Ensure freevxfs kernel module is not available:\n" RESET);
     run_freevxfs_checks();
 
     return 0;
